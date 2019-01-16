6d0ab0c75ca14ce94f704313ff0e4c958acd6938
Grayson Ercanbrack <graymbp@GrayMBP.local>
2019-01-15 20:56:28 -0700

    added button w/js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var title = document.querySelector(".title");
 console.log("the title is", title);
+
+var button = document.querySelector("#button");
+console.log("the button is", button);
+button.onclick = function () {
+    title.style.color = "black";
+}
