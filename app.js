c5213ad7365372843613eb76bdd116eb86e90136
Grayson Ercanbrack <D00340459@dixie.edu>
2019-01-16 12:12:35 -0700

    captured the value for numParty in the console

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ var button = document.querySelector("#button");
 console.log("the button is", button);
 button.onclick = function () {
     title.style.color = "black";
+    var numParty = document.querySelector("#num-party");
+    console.log("the number of party members is", numParty.value);
 }
