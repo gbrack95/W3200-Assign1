0a84b3c11f4cde5450dbaaa3075c14f80729554c
Grayson Ercanbrack <graymbp@GrayMBP.local>
2019-01-18 13:48:40 -0700

    changed to date night, added buttons, fixed styling and updated JS

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var title = document.querySelector("#title");
 console.log("the title is", title);
 
-var button = document.querySelector("#button");
-console.log("the button is", button);
+var activityButton = document.querySelector("#activity-button");
+var foodButton = document.querySelector("#food-button");
+var bothButton = document.querySelector("#both-button");
+console.log("the activity button is", activityButton);
+console.log("the food button is", foodButton);
+console.log("the both button is", bothButton);
+
 button.onclick = function () {
     title.style.color = "black";
     var numParty = document.querySelector("#num-party");
