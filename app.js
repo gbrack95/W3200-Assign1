66dec67409c3cd9a9994c96ce1b55e75b6a6d5a0
Grayson Ercanbrack <graymbp@GrayMBP.local>
2019-01-22 18:27:32 -0700

    added onclick function, changed style and updated html aslo added data

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-var title = document.querySelector("#title");
-console.log("the title is", title);
-
 var activityButton = document.querySelector("#activity-button");
 var foodButton = document.querySelector("#food-button");
 var bothButton = document.querySelector("#both-button");
-console.log("the activity button is", activityButton);
-console.log("the food button is", foodButton);
-console.log("the both button is", bothButton);
 
-button.onclick = function () {
-    title.style.color = "black";
-    var numParty = document.querySelector("#num-party");
-    console.log("the number of party members is", numParty.value);
+var costOption = document.querySelector("#cost-button-container");
+
+var lowCost = document.querySelector("#low");
+var mediumCost = document.querySelector("#medium");
+var highCost = document.querySelector("#high");
+
+activityButton.onclick = function () {
+    console.log("the activity button is", activityButton);
+    costOption.style.display = "block";
+    console.log("display change on activity");
+}
+
+foodButton.onclick = function () {
+    console.log("the food button is", foodButton);
+    costOption.style.display = "block";
+    console.log("display change on food");
+}
+
+bothButton.onclick = function () {
+    console.log("the both button is", bothButton);
+    costOption.style.display = "block";
+    console.log("display change on both");
 }
---
+++ data.js
@@ -0,0 +1,78 @@
+var activity = [{
+    act: "Mini-Golf",
+    cost: "Medium"
+}, {
+    act: "Hiking",
+    cost: "Low"
+}, {
+    act: "Bowling",
+    cost: "High"
+}, {
+    act: "Laser Tag",
+    cost: "Medium"
+}, {
+    act: "Campfire",
+    cost: "Low"
+}, {
+    act: "Game Night",
+    cost: "Low"
+}, {
+    act: "Movie(theater)",
+    cost: "High"
+}, {
+    act: "Movie(home)",
+    cost: "Low"
+}, {
+    act: "Ice Skating",
+    cost: "High"
+}, {
+    act: "Arcade",
+    cost: "Medium"
+}, {
+    act: "Off-Road",
+    cost: "Low"
+}, {
+    act: "Museum",
+    cost: "Medium"
+}];
+
+
+
+var food = [{
+    name: "Grocery Store Sushi",
+    cost: "High"
+}, {
+    name: "Durangos",
+    cost: "Medium"
+}, {
+    name: "McDonalds",
+    cost: "Low"
+}, {
+    name: "Cafe Rio",
+    cost: "High"
+}, {
+    name: "Mad Pita",
+    cost: "Medium"
+}, {
+    name: "Viva Chicken",
+    cost: "High"
+}, {
+    name: "Jimmy Johns",
+    cost: "Medium"
+}, {
+    name: "Cook at Home",
+    cost: "Low"
+}, {
+    name: "Benjas",
+    cost: "High"
+}, {
+    name: "Dairy Queen",
+    cost: "Medium"
+}, {
+    name: "Little Ceasers",
+    cost: "Low"
+}, {
+    name: "El Coyote Charro",
+    cost: "Medium"
+}
+}];
