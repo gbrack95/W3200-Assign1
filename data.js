33e107b0ee32f0e7671ba7f8c4f0c46b7c1b9aa5
Grayson Ercanbrack <graymbp@GrayMBP.local>
2019-02-07 21:10:37 -0700

    Added data

--- data.js
+++ data.js
@@ -55,6 +55,70 @@ var data = // https://api.myjson.com/bins/1ch97g
     {
         "act": "Make Pottery",
         "cost": "High"
+    },
+    {
+        "act": "Hit Golf Balls Off Cliffs",
+        "cost": "Low"
+    },
+    {
+        "act": "Watertower and Chalk",
+        "cost": "Low"
+    },
+    {
+        "act": "Watch Sunset at Chuckawala",
+        "cost": "Low"
+    },
+    {
+        "act": "Night Hike the D",
+        "cost": "Low"
+    },
+    {
+        "act": "Pick Out Movies From Library",
+        "cost": "Low"
+    },
+    {
+        "act": "Bonfire at Pine Valley Spot",
+        "cost": "Low"
+    },
+    {
+        "act": "Ultimate Frisbee",
+        "cost": "Low"
+    },
+    {
+        "act": "Public Festivals",
+        "cost": "Medium"
+    },
+    {
+        "act": "Parking Garage Activities",
+        "cost": "Low"
+    },
+    {
+        "act": "Truth or Dare",
+        "cost": "Low"
+    },
+    {
+        "act": "Public Hide and Seek",
+        "cost": "Low"
+    },
+    {
+        "act": "Makeover Night",
+        "cost": "Low"
+    },
+    {
+        "act": "Painting Night",
+        "cost": "Low"
+    },
+    {
+        "act": "New House Walk Through",
+        "cost": "Low"
+    },
+    {
+        "act": "Spa Night",
+        "cost": "Low"
+    },
+    {
+        "act": "Puzzle Night",
+        "cost": "Low"
     }
 ], "food": [
     {
@@ -116,8 +180,48 @@ var data = // https://api.myjson.com/bins/1ch97g
     {
         "name": "In-N-Out",
         "cost": "Low"
+    },
+    {
+        "name": "Get Fries",
+        "cost": "Low"
+    },
+    {
+        "name": "Zeppes",
+        "cost": "Low"
+    },
+    {
+        "name": "Yogurtland",
+        "cost": "Low"
+    },
+    {
+        "name": "Menchies",
+        "cost": "Low"
+    },
+    {
+        "name": "Pizza or Breadsticks",
+        "cost": "Low"
+    },
+    {
+        "name": "Share Sushi",
+        "cost": "Low"
+    },
+    {
+        "name": "Red Fort",
+        "cost": "High"
+    },
+    {
+        "name": "Casa Dona Maria",
+        "cost": "High"
+    },
+    {
+        "name": "25 Main",
+        "cost": "High"
+    },
+    {
+        "name": "Midnight Denny's",
+        "cost": "Medium"
     }
-]};
+]}
 
 
 
